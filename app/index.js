25d1962cdbead2c1d01a1de57b10f6b5270375d4
v1 <v1@v1s-MacBook-Air.local>
2019-08-20 11:47:56 +0300

    Code splitting

--- app/index.js
+++ app/index.js
@@ -6,10 +6,12 @@ import './index.css';
 
 import { ThemeProvider } from './context/theme';
 
-import Popular from './components/Popular';
-import Battle from './components/Battle';
 import Nav from './components/Nav';
-import Results from './components/Results';
+import Loading from './components/Loading';
+
+const Popular = React.lazy(() => import('./components/Popular'));
+const Battle = React.lazy(() => import('./components/Battle'));
+const Results = React.lazy(() => import('./components/Results'));
 
 class App extends Component {
     state = {
@@ -27,23 +29,25 @@ class App extends Component {
                     <div className={this.state.theme}>
                         <div className="container">
                             <Nav />
-                            <Switch>
-                                <Route
-                                    exact
-                                    path="/"
-                                    component={Popular}
-                                ></Route>
-                                <Route
-                                    exact
-                                    path="/battle"
-                                    component={Battle}
-                                ></Route>
-                                <Route
-                                    path="/battle/results"
-                                    component={Results}
-                                />
-                                <Route render={() => <h1>404</h1>}></Route>
-                            </Switch>
+                            <React.Suspense fallback={<Loading />}>
+                                <Switch>
+                                    <Route
+                                        exact
+                                        path="/"
+                                        component={Popular}
+                                    ></Route>
+                                    <Route
+                                        exact
+                                        path="/battle"
+                                        component={Battle}
+                                    ></Route>
+                                    <Route
+                                        path="/battle/results"
+                                        component={Results}
+                                    />
+                                    <Route render={() => <h1>404</h1>}></Route>
+                                </Switch>
+                            </React.Suspense>
                         </div>
                     </div>
                 </ThemeProvider>
